a3cb8d8a4b68a659f4864bea441b6f03c296fc0f
The Cloud Forest Retreat <thecloudforestretreat@gmail.com>
2026-03-16 11:48:19 -0400

    Update head.js

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,1 +1,22 @@
-console.log("Head loaded");
+// Google Analytics 4 Loader
+
+(function () {
+
+  const GA_ID = "G-1ZYLW22XWP";
+
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_ID}');
+  `;
+
+  document.head.appendChild(script2);
+
+})();
